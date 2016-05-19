554ad3ad5fe295654b9152ee57d8b92d4e4cdabb
Vaibhav Pandey <contact@vaibhavpandey.com>
2016-05-19 02:55:25 -0700

    initialize codebase from samsung archive

---
+++ drivers/sensors/cm36655.h
@@ -0,0 +1,27 @@
+#ifndef __LINUX_CM36655_H
+
+#define __CM36655_H__
+#include <linux/types.h>
+
+#ifdef __KERNEL__
+struct cm36655_platform_data {
+	int irq;		/* proximity-sensor irq gpio */
+	int default_hi_thd;
+	int default_low_thd;
+	int cancel_hi_thd;
+	int cancel_low_thd;
+	int default_trim;
+	int is_defence;
+
+#if defined(CONFIG_SENSORS_CM36655_RESET_DEFENCE_CODE)
+	int pwr_en_gpio;
+#endif
+#if defined(CONFIG_SENSORS_CM36655_SENSOR_EN_GPIO)
+	int sensoren_gpio;
+#endif
+};
+
+extern struct class *sensors_class;
+#endif
+#endif
+
